79e099cdef3b00cebcbc72d95be4b417940190e5
Vladyslav Kolesnychenko <kolesnychenko_v@icloud.com>
2022-05-31 14:52:25 +0300

    subtask 01 finished
    Created markup from HW template and galleryItems massive.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+
+const galleryRef = document.querySelector('div.gallery');
+
+const makeGalleryItemsMarkUp = ({original, preview, description}) => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`;
+}
+
+const makeGalleryMarkUp = galleryItems.map(makeGalleryItemsMarkUp).join('');
+
+galleryRef.insertAdjacentHTML('beforeend', makeGalleryMarkUp);
+
+// Реализация делегирования на div.gallery и получение url большого изображения.
+
