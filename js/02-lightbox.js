e80bedf4cc1050510ec77c3e4568fd2e801f0195
Vladyslav Kolesnychenko <kolesnychenko_v@icloud.com>
2022-06-01 10:26:01 +0300

    task-02 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const makeGalleryItemsMarkUp = ({ original, preview, description }) => {
+  return `<li><a class="gallery__item" href=${original}>
+  <img style="display:block"
+  class="gallery__image"
+  src=${preview}
+  alt=${description}/>
+</a></li>`
+};
+
+const makeGalleryMarkUp = galleryItems.map(makeGalleryItemsMarkUp).join('');
+
+galleryRef.insertAdjacentHTML('beforeend', makeGalleryMarkUp);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+});
